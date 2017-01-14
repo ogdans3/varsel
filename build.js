ef2f4d05b3d988528c8c4420508025679a788a21
Gabriel Berthling-Hansen <gabrielbhansen@hotmail.com>
2017-01-14 17:21:30 +0100

    Added build script and dependencies

---
+++ build.js
@@ -0,0 +1,16 @@
+var minifier = require('minifier');
+var files = ["varsel.js", "varsel.css"];
+
+
+console.log("Minifying files");
+var template = "/dist/{{filename}}.min.{{ext}}";
+for(var i = 0; i < files.length; i++){
+    var result = minifier.generateOutputName(files[i], {template: template});
+    console.log(files[i], "-->", result);
+    minifier.minify(files[i], {output: result});
+}
+
+minifier.on('error', function(err) {
+    console.log("An error occured when we tried to minify a file");
+    console.err(err);
+})
